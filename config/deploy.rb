1956ce372b51d3965ec5d6397ba6251a9ba194d7
Jean-Philippe Bougie <jp.bougie@gmail.com>
2010-06-18 20:59:13 -0400

    removed bluepill, as it's not working on 1.9.2

--- config/deploy.rb
+++ config/deploy.rb
@@ -15,24 +15,3 @@ role :db,  "linode", :primary => true # This is where Rails migrations will run
 #role :db,  "your slave db-server here"
 
 set :deploy_to, "/data/#{application}"
-
-
-
-after "deploy:update", "bluepill:quit", "bluepill:start"
-namespace :bluepill do
-  desc "Stop processes that bluepill is monitoring and quit bluepill"
-  task :quit, :roles => [:app] do
-    sudo "bluepill stop"
-    sudo "bluepill quit"
-  end
- 
-  desc "Load bluepill configuration and start it"
-  task :start, :roles => [:app] do
-    sudo "bluepill load /data/votatoe/current/config/production.pill"
-  end
- 
-  desc "Prints bluepills monitored processes statuses"
-  task :status, :roles => [:app] do
-    sudo "bluepill status"
-  end
-end
