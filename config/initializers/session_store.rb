605afddafcf0092e2f69e476a256016c22681b9a
Jean-Philippe Bougie <jp.bougie@gmail.com>
2010-07-21 20:53:41 -0400

    wrong symbol for the session store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :redis_store
+Rails.application.config.session_store :redis_session_store
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
